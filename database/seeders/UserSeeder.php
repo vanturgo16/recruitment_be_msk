9b91e0a011eca9071391de65207d9dd386f9a70c
Imam Haris Syafaat <38088979+ImamHaris@users.noreply.github.com>
2025-04-10 16:59:43 +0700

    fix seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -29,5 +29,26 @@ class UserSeeder extends Seeder
             'created_at' => '2024-01-20 02:46:46',
             'updated_at' => '2025-04-09 14:53:25',
         ]);
+
+        DB::table('users')->insert([
+            'id' => 2,
+            'name' => 'Admin Recruitment',
+            'email' => 'adminrecruitment@gmail.com',
+            'email_verified_at' => null,
+            'password' => '$2y$12$4KHY6wX4sLr4mBvtT.zhzODoo82v3u1Q.FAXraLseYhnehlZjgtPm',
+            'remember_token' => null,
+            'dealer_type' => 'Main Dealer',
+            'dealer_name' => 'Mitra Sendang Kemakmuran',
+            'department' => 'IT',
+            'role' => 'Admin',
+            'last_login' => null,
+            'last_session' => null,
+            'photo_path' => null,
+            'is_darkmode' => null,
+            'language' => 'en',
+            'is_active' => 1,
+            'created_at' => '2024-01-20 02:46:46',
+            'updated_at' => '2025-04-09 14:53:25',
+        ]);
     }
 }
