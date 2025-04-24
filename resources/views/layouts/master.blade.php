feb651b8791454646b2313763c2857b6f678fbe4
Imam Haris Syafaat <38088979+ImamHaris@users.noreply.github.com>
2025-04-24 11:04:16 +0700

    Adj. Table Scroll

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -351,7 +351,7 @@
                 if (windowHeight < 550) { maxHeight = '10vh'; } 
                 else if (windowHeight < 600) { maxHeight = '19vh'; } 
                 else if (windowHeight < 700) { maxHeight = '29vh'; } 
-                else if (windowHeight < 800) { maxHeight = '36vh'; } 
+                else if (windowHeight < 800) { maxHeight = '35vh'; } 
                 else if (windowHeight < 900) { maxHeight = '42vh'; } 
                 else if (windowHeight < 1000) { maxHeight = '44vh'; }
                 $('#ssTable_wrapper .dataTables_scrollBody').css('max-height', maxHeight);
